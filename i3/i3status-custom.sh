5d739490144bc5bac592890b429c24561ba4921f
RafaelGSS <rafael.nunu@hotmail.com>
2020-01-16 22:35:30 -0300

    Autocommit (2020-01-16 22:35:30)

--- i3/i3status-custom.sh
+++ i3/i3status-custom.sh
@@ -2,5 +2,7 @@
 
 i3status ~/.config/i3/i3status.conf | while :
 do
-    read line
+  read line
+  KEY=$(setxkbmap -print | grep xkb_symbols | awk -F"+" '{print $2}')
+  echo "KEY: $KEY | $line" || exit 1
 done
